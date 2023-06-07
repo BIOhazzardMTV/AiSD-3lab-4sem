fa76fbefa69c877d726a90fc483a517715ffa613
BIOhazzardMTV <fedorov200375@mail.ru>
2023-06-08 02:19:18 +0400

    Adding private method find_by_name and public methods add_vertex, vertices, has_edge

--- AiSD-3/Source.cpp
+++ AiSD-3/Source.cpp
@@ -18,17 +18,30 @@ public:
 
 private:
     std::vector<vertex> graph;
+
+    vertex find_by_name(const V name) {
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph[i].src == name) return graph[i];
+        }
+        return NULL;
+    }
+
 public:
-    //проверка-добавление-удаление вершин
     bool has_vertex(const V& v) const {
         for (int i = 0; i < graph.size(); i++) {
             if (graph[i].src == v) return true;
         }
         return false;
     }
-    void add_vertex(const V& v) {
-        if (!has_vertex(v)) graph.push_back(vertex(v));
+
+    bool add_vertex(const V& v) {
+        if (!has_vertex(v)) {
+            graph.push_back(vertex(v));
+            return true;
+        }
+        return false;
     }
+
     bool remove_vertex(const V& v) {
         if (has_vertex(v)) {
             for (int i = 0; i < graph.size(); i++) {
@@ -39,9 +52,41 @@ public:
                     }
                 }
             }
+            return true;
         }
+        return false;
     }
-    std::vector<V> vertices() const;
+
+    std::vector<V> vertices() const {
+        std::vector<V> res;
+        for (int i = 0; i < graph.size(); i++) {
+            res.push_back(graph[i].src);
+        }
+        return res;
+    }
+
+    bool has_edge (const V src, const V dest) const {
+        vertex a = find_by_name(src);
+        if (a && has_vertex(dest)) {
+            for (int i = 0; i < a.edges.size(); i++) {
+                if (a.edges[i].dest == dest) return true;
+            }
+        }
+        return false;
+    }
+
+    bool has_edge (const V src, const edge e) { //c учетом расстояния в edge
+        vertex a = find_by_name(src);
+        if (a && has_vertex(e.dest)) {
+            for (int i = 0; i < a.edges.size(); i++) {
+                if ((a.edges[i].dest == e.dest) && a.edges[i].dist == e.dist) return true;
+            }
+        }
+        return false;
+    }
+
+
+
 };
 
 int main() {
