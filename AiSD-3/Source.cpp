531e9b6bdcdbb49e9486940768e05d1a47420fb4
BIOhazzardMTV <fedorov200375@mail.ru>
2023-06-08 02:54:32 +0400

    Adding private method index_byname and public method add_edge

--- AiSD-3/Source.cpp
+++ AiSD-3/Source.cpp
@@ -1,31 +1,37 @@
 #include<iostream>
 #include<vector>
-template<typename V, typename Distance = double>
+
+template<typename V, typename D = double>
+struct edge {
+    V dest;
+    D dist;
+    edge(V a, D d) : dest(a), dist(d) {}
+};
+
+template<typename V, typename D = double>
+struct vertex {
+    std::vector<edge<V, D>> edges;  // a.edges({ b,6 }, { c, -5 }, { d, 11 }, { e, -8 })
+    V src;
+    vertex(V n) : src(n) {}
+};
+
+template<typename V, typename D = double>
 class Graph {
-public:
-    struct vertex;
-    struct edge {
-        V dest;
-        Distance dist;
-        edge(V a, Distance d) : dest(a), dist(d) {}
-    }; 
-
-    struct vertex {
-        std::vector<edge> edges;  // a.edges({ b,6 }, { c, -5 }, { d, 11 }, { e, -8 })
-        V src;
-        vertex(V n) : src(n) {}
-    };
-
 private:
-    std::vector<vertex> graph;
+    std::vector<vertex<V, D>> graph;
 
-    vertex find_by_name(const V name) {
+    vertex<V, D> find_by_name(const V name) {
         for (int i = 0; i < graph.size(); i++) {
             if (graph[i].src == name) return graph[i];
         }
         return NULL;
     }
-
+    int index_by_name(const V name) {
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph[i].src == name) return i;
+        }
+        return -1;
+    }
 public:
     bool has_vertex(const V& v) const {
         for (int i = 0; i < graph.size(); i++) {
@@ -75,7 +81,7 @@ public:
         return false;
     }
 
-    bool has_edge (const V src, const edge e) { //c учетом расстояния в edge
+    bool has_edge (const V src, const edge<V, D> e) { //c учетом расстояния в edge
         vertex a = find_by_name(src);
         if (a && has_vertex(e.dest)) {
             for (int i = 0; i < a.edges.size(); i++) {
@@ -85,10 +91,23 @@ public:
         return false;
     }
 
+    bool add_edge(const V src, const V dest, const D& dist) {
+        if (has_vertex(src) && has_vertex(dest)) {
+            if (!has_edge(src, dest)) {
+                graph[index_by_name(src)].edges.push_back(edge<V, D>(dest, dist));
+                return true;
+            }
+        }
+        return false;
+    }
+    bool remove_edge(const V from, const V to);
+    bool remove_edge(const edge<V,D> e); //c учетом расстояния
 
 
 };
 
 int main() {
+    vertex<char> k('a');
+    k.edges.push_back(edge<char, double>('b', 15));
     return 0;
 }
