8e4303457f67c8f46dc255a727d13112e1bea4b7
BIOhazzardMTV <fedorov200375@mail.ru>
2023-06-09 08:04:26 +0400

    New class methods: allEdges(), shortestPath, walk
    New functions: GetT(), AddVertex(), DeleteVertex(), AddEdge()

--- AiSD-3/Source.cpp
+++ AiSD-3/Source.cpp
@@ -1,75 +1,107 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<vector>
 #include<algorithm>
 #include<limits>
 #include<conio.h>
-template<typename V, typename D = double>
-struct edge
-{
+template<typename V, typename D>
+struct edge {
     V dest;
     D dist;
     edge(V a, D d) : dest(a), dist(d) {}
-    bool operator==(edge e)
-    {
-        if ((this->dest == e.dest) && (this->dist == e.dist))
-        {
+    bool operator==(edge e) {
+        if ((this->dest == e.dest) && (this->dist == e.dist)) {
             return true;
         }
         return false;
     }
 };
 
-template<typename V, typename D = double>
-struct vertex
-{
+template<typename V, typename D>
+struct vertex {
     std::vector<edge<V, D>> edges;
     V src;
+    vertex() : src(NULL) {}
     vertex(V n) : src(n) {}
 };
 
 template<typename V, typename D = double>
-class Graph
-{
+class Graph {
 private:
     std::vector<vertex<V, D>> graph;
 
-    int index_by_name(V name) {
-        for (int i = 0; i < graph.size(); i++)
-        {
+    int indexByName(V name) {
+        for (int i = 0; i < graph.size(); i++) {
             if (graph[i].src == name) return i;
         }
         return -1;
     }
 
+    void walkHelp(V src, std::vector<V>& path, std::vector<bool>& visited) {
+        visited[indexByName(src)] = true;
+        path.push_back(src);
+        std::vector<edge<V, D>> adj;
+        for (int i = 0; i < graph[indexByName(src)].edges.size(); i++) {
+            adj.push_back(graph[indexByName(src)].edges[i]);
+        }
+        for (int j = 0; j < adj.size(); j++) {
+            if (!visited[indexByName(adj[j].dest)]) walkHelp(adj[j].dest, path, visited);
+        }
+    }
+
+    D shortestPathHelp(V src, V dest, std::vector<V>& res) {
+        std::vector<D> dis(graph.size(), INT_MAX);
+        std::vector<int> as(graph.size(), -1);
+        dis[indexByName(src)] = 0;
+        for (int i = 0; i < graph.size(); i++) {
+            bool flag = true;
+            for (int j = 0; j < graph.size(); j++) {
+                if (dis[j] != INT_MAX) {
+                    for (int k = 0; k < graph[j].edges.size(); k++) {
+                        if (dis[j] + graph[j].edges[k].dist < dis[indexByName(graph[j].edges[k].dest)]) {
+                            dis[indexByName(graph[j].edges[k].dest)] = dis[j] + graph[j].edges[k].dist;
+                            flag = false;
+                            as[k] = j;
+                        }
+                    }
+                }
+                for (int k = 0; k < dis.size(); k++) {
+                    std::cout << dis[k] << "  ";
+                }
+                std::cout << "\n";
+            }
+            if (i == graph.size() - 1 && !flag) throw "Negative cycle";
+            if (flag) break;
+        }
+        for (V i = dest; i != src; i = graph[as[indexByName(i)]].src) {
+            res.push_back(i);
+        }
+        res.push_back(dest);
+        return dis[indexByName(dest)];
+    }
+
 public:
-    bool has_vertex(const V& v) {
-        for (int i = 0; i < graph.size(); i++)
-        {
+    bool hasVertex(const V& v) {
+        for (int i = 0; i < graph.size(); i++) {
             if (graph[i].src == v) return true;
         }
         return false;
     }
 
-    bool add_vertex(const V& v) {
-        if (!has_vertex(v))
-        {
+    bool addVertex(const V& v) {
+        if (!hasVertex(v)) {
             graph.push_back(vertex<V, D>(v));
             return true;
         }
         return false;
     }
 
-    bool remove_vertex(const V& v)
+    bool removeVertex(const V& v)
     {
-        if (has_vertex(v))
-        {
-            for (int i = 0; i < graph.size(); i++)
-            {
+        if (hasVertex(v)) {
+            for (int i = 0; i < graph.size(); i++) {
                 if (graph[i].src == v) graph.erase(graph.begin() + i);
-                else
-                {
-                    for (int j = 0; j < graph[i].edges.size(); j++)
-                    {
+                else {
+                    for (int j = 0; j < graph[i].edges.size(); j++) {
                         if (graph[i].edges[j].dest == v) graph[i].edges.erase(graph[i].edges.begin() + j);
                     }
                 }
@@ -81,83 +113,80 @@ public:
 
     std::vector<V> vertices() {
         std::vector<V> res;
-        for (int i = 0; i < graph.size(); i++)
-        {
+        for (int i = 0; i < graph.size(); i++) {
             res.push_back(graph[i].src);
         }
         return res;
     }
 
-    bool has_edge(const V src, const V dest) {
-        if (has_vertex(src) && has_vertex(dest))
-        {
-            for (int i = 0; i < graph[index_by_name(src)].edges.size(); i++)
-            {
-                if (graph[index_by_name(src)].edges[i].dest == dest) return true;
+    int allEdges() {
+        int count = 0;
+        for (int i = 0; i < graph.size(); i++) {
+            for (int j = 0; j < graph.size(); j++) {
+                if(hasEdge(graph[i].src, graph[j].src)) count++;
+            }
+        }
+        return count;
+    }
+
+    bool hasEdge(const V src, const V dest) {
+        if (hasVertex(src) && hasVertex(dest)) {
+            for (int i = 0; i < graph[indexByName(src)].edges.size(); i++) {
+                if (graph[indexByName(src)].edges[i].dest == dest) return true;
             }
         }
         return false;
     }
 
-    bool has_edge(const V src, const edge<V, D> e) { //c учетом расстояния в edge
-        if (has_vertex(src) && has_vertex(e.dest))
-        {
-            for (int i = 0; i < graph[index_by_name(src)].edges.size(); i++)
-            {
-                if (graph[index_by_name(src)].edges[i] == e) return true;
+    bool hasEdge(const V src, const edge<V, D> e) { //c СѓС‡РµС‚РѕРј СЂР°СЃСЃС‚РѕСЏРЅРёСЏ РІ edge
+        if (hasVertex(src) && hasVertex(e.dest)) {
+            for (int i = 0; i < graph[indexByName(src)].edges.size(); i++) {
+                if (graph[indexByName(src)].edges[i] == e) return true;
             }
         }
         return false;
     }
 
-    bool add_edge(const V v, const V dest, const D& dist)
-    {
-        if (!has_edge(v, dest))
-        {
-            graph[index_by_name(v)].edges.push_back(edge<V, D>(dest, dist));
+    bool addEdge(const V v, const V dest, const D& dist) {
+        if (!hasEdge(v, dest)) {
+            graph[indexByName(v)].edges.push_back(edge<V, D>(dest, dist));
             return true;
         }
         return false;
     }
 
-    bool remove_edge(const V src, const V dest)
-    {
-        if (has_edge(src, dest)) {
-            for (int i = 0; i < graph[index_by_name(src)].edges.size(); i++)
-            {
-                if (graph[index_by_name(src)].edges[i].dest == dest) graph[index_by_name(src)].edges.erase(graph[index_by_name(src)].edges.begin() + i);
+    bool removeEdge(const V src, const V dest) {
+        if (hasEdge(src, dest)) {
+            for (int i = 0; i < graph[indexByName(src)].edges.size(); i++) {
+                if (graph[indexByName(src)].edges[i].dest == dest) graph[indexByName(src)].edges.erase(graph[indexByName(src)].edges.begin() + i);
                 return true;
             }
         }
         return false;
     }
 
-    bool remove_edge(const V src, const edge<V, D> e) //c учетом расстояния
+    bool removeEdge(const V src, const edge<V, D> e) //c СѓС‡РµС‚РѕРј СЂР°СЃСЃС‚РѕСЏРЅРёСЏ
     {
-        if (has_edge(src, e))
-        {
-            for (int i = 0; i < graph[index_by_name(src)].edges.size(); i++)
-            {
-                if (graph[index_by_name(src)].edges[i] == e) graph[index_by_name(src)].edges.erase(graph[index_by_name(src)].edges.begin() + i);
+        if (hasEdge(src, e)) {
+            for (int i = 0; i < graph[indexByName(src)].edges.size(); i++) {
+                if (graph[indexByName(src)].edges[i] == e) graph[indexByName(src)].edges.erase(graph[indexByName(src)].edges.begin() + i);
                 return true;
             }
         }
         return false;
     }
 
-    std::vector<edge<V, D>> edges(const V src)
-    {
+    std::vector<edge<V, D>> edges(const V src) {
         std::vector<edge<V, D>> res;
-        if (has_vertex(src)){
-            for (int i = 0; i < graph[index_by_name(src)].edges.size(); i++) {
-                res.push_back(graph[index_by_name(src)].edges[i]);
+        if (hasVertex(src)) {
+            for (int i = 0; i < graph[indexByName(src)].edges.size(); i++) {
+                res.push_back(graph[indexByName(src)].edges[i]);
             }
         }
         return res;
     }
 
-    size_t order() const
-    {
+    size_t order() const {
         return graph.size();
     }
 
@@ -166,7 +195,7 @@ public:
         for (int i = 0; i < graph.size(); i++) {
             for (int j = 0; j < graph[i].edges.size(); j++) {
                 count[i]++;
-                count[index_by_name(graph[i].edges[j].dest)]++;
+                count[indexByName(graph[i].edges[j].dest)]++;
             }
         }
         int max = count[0];
@@ -177,147 +206,184 @@ public:
         return max;
     }
 
-    void print() const
-    {
-        for (int i = 0; i < graph.size(); i++)
-        {
+    void print() const {
+        for (int i = 0; i < graph.size(); i++) {
             std::cout << "[" << graph[i].src << "]";
-            for (int j = 0; j < graph[i].edges.size(); j++)
-            {
+            for (int j = 0; j < graph[i].edges.size(); j++) {
                 std::cout << "->[" << graph[i].edges[j].dest << "]";
             }
             std::cout << "\n";
         }
     }
 
-    std::vector<V>  walk_2(const V& p, std::vector<bool>& labels) const
-    {
-        std::vector<V> path;
-        vertex<V, D> tmp = find_by_name(p);
-        path.push_back(p);
-        labels[index_by_name(p)] = true;
-        for (int i = 0; i < tmp.edges.size(); i++)
-        {
-            if (!labels[index_by_name(tmp.edges[i].dest)])
-            {
-                std::vector<V> path_2 = walk_2(tmp.edges[i].dest, labels);
-                for (int j = 0; j < path_2.size(); j++)
-                {
-                    path.push_back(path_2[j]);
-                }
-            }
+    std::pair<std::vector<V>,D> shortestPath(const V src, const V dest) {
+        std::vector<V> res;
+        D dist = shortestPathHelp(src, dest, res);
+        return std::pair<std::vector<V>,D>(res, dist);
+    }
+
+    std::vector<V> walk(V src) {
+        std::vector<bool> visited;
+        for (int i = 0; i < graph.size(); i++) {
+            visited.push_back(false);
         }
+        std::vector<V> path;
+        walkHelp(src, path, visited);
         return path;
     }
-    std::vector<V>  walk1(const V& start_vertex) const
-    {
-        if (!has_vertex(start_vertex)) throw "Error";
-        std::vector<V> path;
-        std::vector<bool> labels(graph.size(), false);
-        V p = start_vertex;
-        while (true)
-        {
-            std::vector<V> path_2 = walk_2(p, labels);
-            for (int j = 0; j < path_2.size(); j++)
-            {
-                path.push_back(path_2[j]);
-            }
-            bool f = true;
-            for (int i = 0; i < graph.size(); i++)
-            {
-                if (labels[i] == false)
-                {
-                    p = graph[i].src;
-                    f = false;
-                    break;
-                }
-            }
-            if (f) break;
-        }
-        return path;
-    }
+
 };
 
+void IgnoreLine() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+template<typename T>
+T GetT() {
+    while (true) {
+        T x{};
+        std::cin >> x;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            IgnoreLine();
+            system("cls");
+            std::cout << "Oops, that input is invalid.  Please try again.\n" << std::endl;
+        }
+        else {
+            IgnoreLine();
+            return x;
+        }
+    }
+}
+
+template<typename V,typename D>
+void AddVertex(Graph<V,D>& g) {
+    std::cout << "Enter the name of vertex: ";
+    V name = GetT<V>();
+    while (g.hasVertex(name)) {
+        std::cout << "\n";
+        std::cout << "Oops, that input is invalid.  Please try again: " << std::endl;
+        name = GetT<V>();
+    }
+    g.addVertex(name);
+}
+
 template<typename V, typename D>
-Graph<V, D> WorkWithGraph() {
-    Graph<V, D> g;
-    g.add_vertex(1);
-    g.add_vertex(2);
-    g.add_edge(1,2,5);
-    g.print();
-    if (_getch());
-    return g;
+void DeleteVertex(Graph<V, D>& g) {
+    if (g.vertices().size() == 0) {
+        std::cout << "Available vertices:" << std::endl;
+        for (int i = 0; i < g.vertices().size(); i++) {
+            std::cout << g.vertices()[i] << "  ";
+        }
+        std::cout << "\nEnter the name of vertex: ";
+        V name = GetT<V>();
+        while (!g.hasVertex(name)) {
+            std::cout << "\n";
+            std::cout << "Oops, that input is invalid.  Please try again: " << std::endl;
+            name = GetT<V>();
+        }
+        g.removeVertex(name);
+    }
+}
+
+template<typename V, typename D>
+void AddEdge(Graph<V, D>& g) {
+    if (g.vertices().size() > 1) {
+        std::cout << "Enter the start vertex: ";
+        V name1 = GetT<V>();
+        while (!g.hasVertex(name1)) {
+            std::cout << "\n";
+            std::cout << "Oops, that input is invalid.  Please try again: ";
+            name1 = GetT<V>();
+        }
+        std::cout << "Enter the end vertex: ";
+        V name2 = GetT<V>();
+        while (!g.hasVertex(name2)) {
+            std::cout << "\n";
+            std::cout << "Oops, that input is invalid.  Please try again: ";
+            name2 = GetT<V>();
+        }
+        std::cout << "Enter the weight of the edge: ";
+        D dist = GetT<D>();
+        while (dist == 0) {
+            std::cout << "\n";
+            std::cout << "Oops, that input is invalid.  Please try again: ";
+            dist = GetT<D>();
+            std::cout << "\n";
+        }
+        g.addEdge(name1, name2, dist);
+    }
+    else std::cout << "Your graph is empty." << std::endl;
 }
 
 int main() { 
-    Graph<int, int> g;
-    for (int i = 0; i < 7; i++) {
-        g.add_vertex(i);
-    }
-    for (int i = 0; i < 6; i++) {
-        g.add_edge(i, i + 1, i);
-    }
-    g.print();
-    std::cout << std::endl;
-    std::cout << g.degree() << "  " << g.order();
-    g.edges(1);
-    g.remove_edge(1, g.edges(1)[0]);
-    g.remove_vertex(6);
-    g.print();
-    std::cout << std::endl;
-    std::vector<int> kk = g.vertices();
-    for (int i = 0; i < kk.size(); i++) {
-        std::cout << kk[i] << " ";
-    }
-    /*Graph<int, int> g;
+    Graph<char*, int> g;
     while (true) {
         system("cls");
-        std::cout << "1 - Work with graph" << std::endl;
-        std::cout << "2 - " << std::endl;
-        std::cout << "3 - " << std::endl;
-        std::cout << "4 - " << std::endl;
-        std::cout << "5 - " << std::endl;
-        std::cout << "6 - " << std::endl;
-        std::cout << "7 - " << std::endl;
-        std::cout << "8 - " << std::endl;
+        std::cout << "1 - Add vertex" << std::endl;
+        std::cout << "2 - Remove vertex" << std::endl;
+        std::cout << "3 - Add edge" << std::endl;
+        std::cout << "4 - Remove edge" << std::endl;
+        std::cout << "5 - Check for vertex" << std::endl;
+        std::cout << "6 - Check for edge" << std::endl;
+        std::cout << "7 - Get vertices" << std::endl;
+        std::cout << "8 - Get order" << std::endl;
+        std::cout << "9 - Get degree" << std::endl;
+        std::cout << "10 - Find shortest path between 2 vertices" << std::endl;
+        std::cout << "11 - Walk through graph" << std::endl;
+        std::cout << "12 - Task" << std::endl;
         std::cout << "0 - Exit the program" << std::endl;
-        int ans = _getch();
+        int ans = GetT<int>();
         system("cls");
         switch (ans) {
-        case '1':
-            g = WorkWithGraph<int, int>();
+        case 1:
+            AddVertex(g);
             break;
-        case '2':
+        case 2:
+            DeleteVertex(g);
             break;
-        case '3':
+        case 3:
+            AddEdge(g);
             std::cout << "\nPress any key to continue..." << std::endl;
             if (_getch());
-        case '4':
-            std::cout << "\nPress any key to continue..." << std::endl;
-            if (_getch());
-            break;
-        case '5':
-            std::cout << "\nPress any key to continue..." << std::endl;
-            if (_getch());
-            break;
-        case '6':
+        case 4:
             std::cout << "\nPress any key to continue..." << std::endl;
             if (_getch());
             break;
-        case '7':
+        case 5:
             std::cout << "\nPress any key to continue..." << std::endl;
             if (_getch());
             break;
-        case '8':
+        case 6:
             std::cout << "\nPress any key to continue..." << std::endl;
             if (_getch());
             break;
-        case '0':
+        case 7:
+            std::cout << "\nPress any key to continue..." << std::endl;
+            if (_getch());
+            break;
+        case 8:
+            std::cout << "\nPress any key to continue..." << std::endl;
+            if (_getch());
+            break;
+        case 9:
+
+            break;
+        case 10:
+
+            break;
+        case 11:
+
+            break;
+        case 12:
+
+            break;
+        case 0:
             std::cout << "See you soon :)" << std::endl;
             return 0;
             break;
         default:
             break;
         }
-    }*/
+    }
 }
